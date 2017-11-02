a263b586d2a5fdb41d3c5e913777cb57a701566a
unknown <himanipanwar34@gmail.com>
2017-11-03 03:46:51 +0530

    final update

---
+++ js/LogIn_3.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+       $("#submit").click(function(){
+       var url= 'flourish-release.ap-south-1.elasticbeanstalk.com/stocks/checkLogin';
+        $.ajax({
+          type: 'POST',
+          url: url,
+          data: {username: $("#username").val() , password: $("#password").val()},
+          success: function(wb){
+              console.log(wb);
+          }
+        });
+        window.location.href="./getStock.html";
+       });
+});
---
+++ js/main_cat.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+        var url= 'http://flourish-release.ap-south-1.elasticbeanstalk.com/stocks/getMainCategory';
+        $.ajax({
+          type: 'POST',
+          url: url,
+          // send the id from localstorage to API.
+          data: {id: localStorage.getItem('warehouse_Loc') },
+          success: function(wb){
+            console.log(wb);
+          }
+        });
+        $.getJSON('http://flourish-release.ap-south-1.elasticbeanstalk.com/stocks/getMainCategory',
+          function(wb){
+            for(i in wb.allcat){
+              var maincat = wb.allcat[i];
+              console.log(maincat);
+              $("#maincat_row").append('<div id="columns" class="col-md-4 col-md-4 col-xs-12 thumb">\
+                <p id="result"><button class="main_item" id='+maincat.id+'>'+ maincat.name + '</button></p>\
+                </div>');
+            } 
+          }
+        );
+        $(document).on("click", ".main_item", function(e){
+          var id = $(this).attr('id');
+          // store ID of main category in storage
+          localStorage.setItem('main_cat',id);
+          window.location.href = "./sub_cat.html";
+        });
+        $("#textInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#maincat_row div#columns").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+          });
+        });
+});
